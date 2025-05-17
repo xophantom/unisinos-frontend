e932212dae3a469eca4ed014f0fdc441353f8ffc
Leo Sperandio <leomartins64@yahoo.com.br>
2025-05-17 10:01:39 -0300

    refactor: change step tie braker texts

--- src/components/steps/StepTiebreaker.tsx
+++ src/components/steps/StepTiebreaker.tsx
@@ -1,24 +1,28 @@
-import React from 'react';
-import { usePoints, SCHOOL_COLORS } from '@/context/PointsContext';
-import ColorBox from '@/components/common/ColorBox';
+import React from "react";
+import { usePoints, SCHOOL_COLORS } from "@/context/PointsContext";
+import ColorBox from "@/components/common/ColorBox";
 
 const StepTiebreaker = () => {
   const { schoolMatches, setSchool, setCourses, setStep } = usePoints();
 
-  const handleSchoolSelect = (schoolMatch: typeof schoolMatches[0]) => {
+  const handleSchoolSelect = (schoolMatch: (typeof schoolMatches)[0]) => {
     setSchool(schoolMatch.school);
     setCourses(schoolMatch.courses);
     setStep(1);
   };
 
   return (
-    <div style={{width: '70%'}} className="relative ml-24 mr-24">
+    <div style={{ width: "70%" }} className="relative ml-24 mr-24">
       <div className="bg-white rounded-2xl p-6 flex flex-col items-center shadow-lg">
-      <h1 className="text-2xl font-bold text-center mb-6">
-          Escolha uma Escola
+        <h1
+          className="text-2xl font-bold text-center"
+          style={{ color: "#194db2" }}
+        >
+          Descubra a característica que mais representa você:
         </h1>
+
         <p className="text-center text-gray-600 mb-8">
-          Identificamos um empate entre algumas escolas. Por favor, escolha a escola que mais te interessa:
+        Leia com atenção e escolha o que mais combina com o seu jeito de pensar, agir e se expressar.
         </p>
         <div className="flex flex-col items-center space-y-4">
           {schoolMatches.map((match) => (
@@ -35,4 +39,4 @@ const StepTiebreaker = () => {
   );
 };
 
-export default StepTiebreaker; 
+export default StepTiebreaker;
